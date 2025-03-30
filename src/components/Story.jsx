44fbc2e5abcf9f6a4566a31f84a30955afe3d856
Colin Huber <cbhuber@gmail.com>
2025-03-30 14:12:28 -0600

    fix: Story image location

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -67,7 +67,7 @@ const FloatingImage = () => {
                   onMouseLeave={handleMouseLeave}
                   onMouseUp={handleMouseLeave}
                   onMouseEnter={handleMouseLeave}
-                  src="/img/entrance.webp"
+                  src="/zentry-website/img/entrance.webp"
                   alt="entrance.webp"
                   className="object-contain"
                 />
